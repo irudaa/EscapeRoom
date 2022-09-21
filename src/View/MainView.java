369bcd8826e224bd086b5f3d6c69fdf85e374ddf
hiba <hibaslimani001@gmail.com>
2022-09-21 13:46:59 +0200

    this is real, this is me

--- src/View/MainView.java
+++ src/View/MainView.java
@@ -10,13 +10,13 @@ public class MainView extends JFrame {
     private FourthAngle room;
     public MainView(){
         super("Let me Out!");
-        initGUI();
         frame = new JFrame();
+        mainPanel = new JPanel();
+        initGUI();
     }
 
     private void initGUI(){
         //setting main qualities in place
-        JPanel mainPanel = new JPanel();
         frame.setPreferredSize(new Dimension(1200, 1000));
         frame.setMinimumSize(new Dimension(400, 400));
         frame.setMaximumSize(new Dimension(1400, 1200));
@@ -27,31 +27,42 @@ public class MainView extends JFrame {
 
         //First Window shown
 
+        addFirstWindow();
+        addInventory();
+        addDoorRoom();
 
-        //addInventory();
-        //addDoor + Room
+        frame.add(mainPanel, BorderLayout.CENTER);
+        frame.setVisible(true);
+        frame.pack();
+    }
 
-        //Inventory
+
+    public void addFirstWindow(){
+
+
+    }
+
+    public void addInventory(){
+        //Create inventory
         JPanel inventory = new JPanel();
         inventory.setPreferredSize(new Dimension(100, 600));
         Box inv = Box.createVerticalBox();
-       // inventory.setLayout(new GridLayout(3, 1));
 
+        //First Button
         Button item1 = new Button("button1");
         item1.setPreferredSize(new Dimension(100, 100));
         inv.add(item1);
         inventory.add(inv);
 
-        //AddDoor+Room
+        mainPanel.add(inventory, BorderLayout.WEST);
+    }
+
+    public void addDoorRoom(){
         ImageIcon doorImg = new ImageIcon(new ImageIcon("/home/hiba/ClassesM2/advUI/EscapeRoom/Images/door-146676_640 1.png").getImage().getScaledInstance(200, 400, Image.SCALE_DEFAULT));
         ImageIcon roomImg = new ImageIcon(new ImageIcon("/home/hiba/ClassesM2/advUI/EscapeRoom/Images/Angle 1.png").getImage().getScaledInstance(800, 600, Image.SCALE_DEFAULT));
         room = new FourthAngle(mainPanel, doorImg, roomImg);
         room.addRoom();
-       // room.addDoor();
-        mainPanel.add(inventory, BorderLayout.WEST);
-        frame.add(mainPanel, BorderLayout.CENTER);
-        frame.setVisible(true);
-        frame.pack();
+        // room.addDoor();
     }
 
 
